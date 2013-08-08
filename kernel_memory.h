f462e99bb907f7590e143de9230135a32b5997ee
fi01 <furu.is01@gmail.com>
2013-08-08 10:35:04 +0900

    Reduce kernel memory size to 32MB

--- kernel_memory.h
+++ kernel_memory.h
@@ -1,7 +1,7 @@
 #include <stdbool.h>
 
 #define KERNEL_BASE_ADDRESS             0xc0008000
-#define KERNEL_MEMORY_SIZE              0x0a000000
+#define KERNEL_MEMORY_SIZE              0x02000000
 
 extern void *convert_to_kernel_virtual_address(void *address);
 extern void *convert_to_kernel_mapped_address(void *address);
